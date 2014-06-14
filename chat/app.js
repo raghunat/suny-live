ae013fd06e59495d98251e933387ce06e5e167e6
Professor Raghunath <raghunat@fredonia.edu>
2014-06-14 15:52:56 -0400

    Added Mongoose to modules for both servers
    Altered chat server to support transparent start with port argument

--- chat/app.js
+++ chat/app.js
@@ -1,14 +1,13 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
+var express = require('express'),
+    path = require('path'),
+    favicon = require('static-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    app = express(),
+    http = require("http").Server(app),
+    io = require("socket.io")(http),
+    mongoose = require("mongoose");
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,9 +19,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -55,5 +51,12 @@ app.use(function(err, req, res, next) {
     });
 });
 
+//ROUTES
+//TODO
 
-module.exports = app;
+
+
+//launch the server
+http.listen(process.argv[2], function(){
+    console.log("Chat Instance started at port: " + process.argv[2]);
+});
