0300743e91396540e4171da3e4ff0bfabae544ca
Professor Raghunath <raghunat@fredonia.edu>
2014-06-14 21:58:07 -0400

    Added View template for the chat room
    Added view template for the profile page in the portal server
    
    FROM MARCOS

--- portal/routes/index.js
+++ portal/routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET profile page */
+//Testing Object Below
+var user = {
+    name : "Marcos",
+    email  : "maupaso@gmail.com",
+    userName : "maupaso",
+    password : "test",
+    IP : "192.168.0.103",
+    type : 5
+};
+
+router.get('/profile', function(req, res){
+    res.render('profile', {user: user});
+});
+
+
 module.exports = router;
