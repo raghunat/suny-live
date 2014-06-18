034d6a9f7edc24e52128c4a513605868fbc7d666
Jessiel Heitor Hacke <jessielhh@gmail.com>
2014-06-18 11:35:01 -0400

    MessageController updated

--- chat/bin/DBControllers/messageSchema.js
+++ chat/bin/DBControllers/messageSchema.js
@@ -4,8 +4,8 @@ var messageSchema = mongoose.Schema({
 	message : String,  //Message send in the chat
 	user_from : String, //Email address of the user sending the message
 	user_to : String, //Email address of the user receiving 
-	pub : boolean, //Public(broadcast the message) or Private(send to a determined user)
-	chatRoom  : {type: Schema.ObjectId, ref: 'ChatRoom'}, //ChatRoom link
+	pub : Boolean, //Public(broadcast the message) or Private(send to a determined user)
+	chatRoom  : {type: String, ref: 'ChatRoom'}, //ChatRoom link Schema.ObjectId
 	date : Date //Message's date time
 });
 
