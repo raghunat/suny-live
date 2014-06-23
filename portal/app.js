1f07fa611885e8ea22aadd8c2a745ef271a05092
Professor Raghunath <raghunat@fredonia.edu>
2014-06-22 21:17:49 -0400

    Adding Portal changes and chat changes

---
+++ portal/app.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var routes = require('./routes/index');
+
+var app = express();
+
+//connect to mongo
+mongoose.connect("mongodb://publicUser:publicUser@ds043027.mongolab.com:43027/csit390test");
+var db = mongoose.connection;
+mongoose.connection.on('error', function(err) {
+    console.error('MongoDB error: %s', err);
+});
+require('./bin/passport')(passport);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+require('./routes')(app, passport); // load our routes and pass in our app and fully configured passport
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+db.once("open", function(){
+    app.listen(8080, function(){
+        console.log("Portal Server is running on Port 8080");
+    });
+});
+
---
+++ portal/bin/passport/config/database.js
@@ -0,0 +1,5 @@
+// config/database.js
+module.exports = {
+
+	'url' : "mongodb://publicUser:publicUser@ds043027.mongolab.com:43027/csit390test"
+};
